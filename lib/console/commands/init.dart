7b4a36da27855767ec8289b04764bbeae1a3103c
The Full-Stack Jonin <francis94c@gmail.com>
2026-09-17 15:42:02 +0100

    feat: Enhance init command to manage .gitignore for local dependencies

--- lib/console/commands/init.dart
+++ lib/console/commands/init.dart
@@ -32,6 +32,35 @@ dev_dependencies:
       return;
     }
 
+    final gitIgnoreFile = File('.gitignore');
+
+    if (!gitIgnoreFile.existsSync()) {
+      gitIgnoreFile.createSync();
+      gitIgnoreFile.writeAsStringSync(
+        'local-deps.yaml\n',
+        mode: FileMode.append,
+      );
+      gitIgnoreFile.writeAsStringSync('deps-bkp.yaml\n', mode: FileMode.append);
+      logger.success(
+        'Updated .gitignore with local-deps.yaml and deps-bkp.yaml',
+      );
+    } else {
+      if (!gitIgnoreFile.readAsStringSync().contains('local-deps.yaml')) {
+        gitIgnoreFile.writeAsStringSync(
+          'local-deps.yaml\n',
+          mode: FileMode.append,
+        );
+        logger.success('Updated .gitignore with local-deps.yaml');
+      }
+      if (!gitIgnoreFile.readAsStringSync().contains('deps-bkp.yaml')) {
+        gitIgnoreFile.writeAsStringSync(
+          'deps-bkp.yaml\n',
+          mode: FileMode.append,
+        );
+        logger.success('Updated .gitignore with deps-bkp.yaml');
+      }
+    }
+
     logger.warning(
       'Local substitution file already exists: ${localSubstitutionFile.path}',
     );
